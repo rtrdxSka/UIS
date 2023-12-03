fa538a575767f9e502e8221169abb597adf111dc
Alex Traykov <alextraikov02@gmail.com>
2023-12-03 21:59:37 +0200

    Created a static class MoodleAuthConstants.

---
+++ UIS.API/UIS.DAL/Constants/MoodleAuthConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace UIS.DAL.Constants
+{
+    public static class MoodleAuthConstants
+    {
+        public static string ClientId = "ClientId1";
+        public static string ClientSecret = "92ad170a2d2e41c0dc0fea1d6280a9ce99b4cc885bc04938";
+        public static string GrantType = "authorization_code";
+        public static string Scope = "user_info";
+        public static string TokenUrl = "http://localhost/local/oauth/token.php";
+        public static string UserInfoUrl = "http://localhost/local/oauth/user_info.php";
+        public static string RestAPIUrl = "http://localhost/webservice/rest/server.php";
+    }
+}
