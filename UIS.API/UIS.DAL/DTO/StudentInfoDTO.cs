7865d6b4f32eb8715e2e1b7bafed62609de2e1d6
Martin Petrov <martuto31@gmail.com>
2023-12-07 00:35:36 +0200

    Added functionallity to sync moodle cohorts.

---
+++ UIS.API/UIS.DAL/DTO/StudentInfoDTO.cs
@@ -0,0 +1,25 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace UIS.DAL.DTO
+{
+    public class StudentInfoDTO
+    {
+        [Name("username")]
+        public string Username { get; set; }
+
+        [Name("firstname")]
+        public string FirstName { get; set; }
+
+        [Name("middlename")]
+        public string MiddleName { get; set; }
+
+        [Name("lastname")]
+        public string LastName { get; set; }
+
+        [Name("email")]
+        public string Email { get; set; }
+
+        [Name("cohort1")]
+        public string Cohort1 { get; set; }
+    }
+}
