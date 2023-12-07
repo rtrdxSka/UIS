f98990d0df2c30f5595d60d5c814291d619f8f97
Alex Traykov <alextraikov02@gmail.com>
2023-12-07 20:14:49 +0200

    Setup Database

---
+++ UIS.API/UIS.DATA/Database/UisDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace UIS.DATA.Database
+{
+    public class UisDbContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+        public UisDbContext(IConfiguration iconfig)
+        {
+            _configuration = iconfig;
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<StudentInfo>()
+                .HasMany(s => s.DiscordData)
+                .WithOne(e => e.StudentInfo)
+                .HasForeignKey(e => e.StudentId)
+                .IsRequired();
+
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = _configuration.GetConnectionString("uisStudent");
+            optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        }
+        
+        public DbSet<StudentInfo> Students { get; set; } = null!;
+        public DbSet<DiscordData> DiscordData { get; set; } = null!;
+    }
+}
---
+++ UIS.API/UIS.DATA/DiscordData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UIS.DATA
+{
+    public class DiscordData
+    {
+        public int Id { get; set; }
+        public int StudentId { get; set; }
+        public string DiscordId { get; set; } = null!;
+        public string GuildId { get; set; } = null!;
+        public StudentInfo StudentInfo { get; set; } = null!;
+    }
+}
