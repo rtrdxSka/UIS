911e2f02ebca6b66dd0d79ba11360f1c4d40c101
Martin Petrov <martuto31@gmail.com>
2023-12-11 01:16:13 +0200

    GetMoodleUpdataData in CohortService now returns all students from the selected cohort; Added UI for uploading CSV file and retrieving moodle update info;

--- UIS.API/UIS.DAL/DTO/CohortUpdateDataDTO.cs
+++ UIS.API/UIS.DAL/DTO/CohortUpdateDataDTO.cs
@@ -3,7 +3,9 @@
     public class CohortUpdateDataDTO
     {
         public int CohortId { get; set; } 
+        public string CohortName { get; set; }
         public List<StudentInfoDTO>? StudentsToRemoveFromCohort { get; set; }
         public List<StudentInfoDTO>? StudentsToAddToCohort { get; set; }
+        public List<StudentInfoDTO>? AllStudents { get; set; }
     }
 }
