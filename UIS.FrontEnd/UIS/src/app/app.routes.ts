b1b2613301d838b5dc2d45eccc0244f153e42e72
Martin Petrov <martuto31@gmail.com>
2023-12-17 17:19:23 +0200

    Updated UI

--- UIS.FrontEnd/UIS/src/app/app.routes.ts
+++ UIS.FrontEnd/UIS/src/app/app.routes.ts
@@ -1,5 +1,8 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    
+    {
+        path: '',
+        loadComponent: () => import('./components/moodle-sync/moodle-sync.component').then(m => m.MoodleSyncComponent)
+    }
 ];
