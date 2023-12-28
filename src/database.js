74f03643184c4b8659b70ae4242af2b833b4fa45
Alex Traykov <alextraikov02@gmail.com>
2023-12-28 18:09:24 +0200

    Modified database.js file

--- src/database.js
+++ src/database.js
@@ -1,16 +1,21 @@
 const mysql = require('mysql2');
 // Async function to execute the query with parameters
 // Function to execute the query with parameters using Promise
+function createConnection() {
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 4300,
+    user: 'root',
+    password: 'admin',
+    database: 'uis_student'
+  });
+  return connection;
+}
+
 function executeQuery(facultyNumber, guildId) {
   return new Promise((resolve, reject) => {
     // Create a connection to the database
-    const connection = mysql.createConnection({
-      host: 'localhost',
-      port: 4300, // Port should be a number, not a string
-      user: 'root',
-      password: 'admin',
-      database: 'uis_student'
-    });
+    const connection = createConnection();
 
     // SQL query
     const query = `
@@ -42,13 +47,7 @@ function executeQuery(facultyNumber, guildId) {
 }
 
 async function facultyDB(faculty) {
-  const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 4300, // Port should be a number, not a string
-    user: 'root',
-    password: 'admin',
-    database: 'uis_student'
-  });
+  const connection = createConnection();
 
   // Return a new Promise
   return new Promise((resolve, reject) => {
@@ -71,13 +70,7 @@ async function facultyDB(faculty) {
 }
 
 async function discrordDBCheck(facultyNumber){
-  const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 4300, // Port should be a number, not a string
-    user: 'root',
-    password: 'admin',
-    database: 'uis_student'
-  });
+  const connection = createConnection();
 
   return new Promise((resolve, reject) =>{
     connection.connect();
@@ -102,13 +95,7 @@ async function discrordDBCheck(facultyNumber){
 }
 
 function checkStudentAuth(discordID){
-  const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 4300, // Port should be a number, not a string
-    user: 'root',
-    password: 'admin',
-    database: 'uis_student'
-  });
+  const connection = createConnection();
 
   return new Promise((resolve, reject) =>{
     connection.connect();
@@ -129,19 +116,11 @@ function checkStudentAuth(discordID){
     )
 
   })
-
-
 }
 
 // Function to insert data into discorddata table
 function insertIntoDiscordData(discordData) {
-  const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 4300, // Port should be a number, not a string
-    user: 'root',
-    password: 'admin',
-    database: 'uis_student'
-  });
+  const connection = createConnection();
 
   const sql = 'INSERT INTO discorddata SET ?';
   connection.query(sql, discordData, (error, results) => {
